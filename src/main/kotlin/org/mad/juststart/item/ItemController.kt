67b5d2bd20e5e7658632b78a1e9eb9c9efcabcf6
Yu, Nan <aaronyu@live.com>
2019-08-20 23:49:28 +0800

    Add item controller.

---
+++ src/main/kotlin/org/mad/juststart/item/ItemController.kt
@@ -0,0 +1,12 @@
+package org.mad.juststart.item
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class ItemController {
+    @GetMapping("/items")
+    fun getItems(): Array<String> {
+        return arrayOf("apple", "mongo");
+    }
+}
